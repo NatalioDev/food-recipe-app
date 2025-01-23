7dbc6faf2d9fbb23991c2d3043fb40d60ee32736
NatalioDev <nataliodg.dev@gmail.com>
2025-01-23 20:35:42 -0300

    types

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -11,3 +11,30 @@ export interface RecipeResponse {
         recipes: Recipe[];
     };
 }
+
+export interface RecipeItem {
+  item: Recipe | null;
+}
+
+
+// Definir el tipo del estado extendido con `ingredients`
+export interface RecipeDetails extends Recipe {
+  ingredients: {
+      quantity: number | null;
+      unit: string;
+      description: string;
+  }[];
+}
+
+// Interfaz para definir la estructura de la respuesta a la API
+export interface RecipeDetailsResponse {
+    data:{
+      recipe: Recipe & {
+        ingredients: {
+          quantity: number | null;
+          unit: string;
+          description: string;
+        }[];
+      };
+    };
+  };
